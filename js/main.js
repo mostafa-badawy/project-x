385ec840486fcb656bd0155dae565262de1d9a58
Darsh1111 <lol_mr007@hotmail.com>
2018-07-14 11:55:13 +0200

    git files update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
-var a = 2;
-if (a >= 1){
-    console.log("true");
-}else if(a < 3){
-    console.log("true");
-}else{
-    console.log("false");
-}
+$(document).ready(function(){
+    $('.slider').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 4,
+        responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+            arrows: true,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 4
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+            arrows: true,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+            }
+        }
+        ]
+    });
+});
